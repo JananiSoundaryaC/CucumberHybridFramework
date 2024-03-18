5739f08ac05585a3d0d1fa1ff99810d5f8faeb25
janani <jananisoundaryac@gmail.com>
2024-03-18 22:05:19 +0000

    adding loginpage,feature and stepdefenition of login page

---
+++ src/test/java/com/bank/StepDefs/LoginPageStepDefenition.java
@@ -0,0 +1,112 @@
+package com.bank.StepDefs;
+
+public class LoginPageStepDefenition {
+
+    @Given("User is on the Customer login page")
+    public void user_is_on_the_customer_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters valid Username TestUser2")
+    public void user_enters_valid_username_test_user2() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters valid Password Test@{int}")
+    public void user_enters_valid_password_test(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should be navigated to Accounts Services Page")
+    public void user_should_be_navigated_to_accounts_services_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    Step skipped
+
+    Step skipped
+
+    Step skipped
+
+    Step undefined
+    You can implement this step and 3 other step(s) using the snippet(s) below:
+
+    @Given("User is on the Customer login page")
+    public void user_is_on_the_customer_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters valid Username TestUser2")
+    public void user_enters_valid_username_test_user2() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters invalid Password Test@{int}")
+    public void user_enters_invalid_password_test(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should be prompted with Error The username and password could not be verified")
+    public void user_should_be_prompted_with_error_the_username_and_password_could_not_be_verified() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    Step skipped
+
+    Step skipped
+
+    Step skipped
+
+    Undefined scenarios:
+    file:///C:/Users/janan/IdeaProjects/HybridCucumberFramework/src/test/resources/features/LoginPageFeature.feature:3 # Login with correct credentials
+    file:///C:/Users/janan/IdeaProjects/HybridCucumberFramework/src/test/resources/features/LoginPageFeature.feature:9 # Login with invalid password
+
+            2 Scenarios (2 undefined)
+8 Steps (6 skipped, 2 undefined)
+0m0.600s
+
+
+    You can implement missing steps with the snippets below:
+
+    @Given("User is on the Customer login page")
+    public void user_is_on_the_customer_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters valid Username TestUser2")
+    public void user_enters_valid_username_test_user2() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters valid Password Test@{int}")
+    public void user_enters_valid_password_test(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should be navigated to Accounts Services Page")
+    public void user_should_be_navigated_to_accounts_services_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters invalid Password Test@{int}")
+    public void user_enters_invalid_password_test(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should be prompted with Error The username and password could not be verified")
+    public void user_should_be_prompted_with_error_the_username_and_password_could_not_be_verified() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
--- src/test/resources/features/LoginPageFeature.feature
+++ src/test/resources/features/LoginPageFeature.feature
@@ -2,12 +2,12 @@ Feature: To test the login functionality
 
   Scenario: Login with correct credentials
     Given User is on the Customer login page
-    When User enters valid Username
-    And User enters valid Password
+    When User enters valid Username TestUser2
+    And User enters valid Password Test@1230
     Then User should be navigated to Accounts Services Page
 
     Scenario: Login with invalid password
       Given User is on the Customer login page
-      When User enters valid Username
-      And User enters invalid Password
+      When User enters valid Username TestUser2
+      And User enters invalid Password Test@123
       Then User should be prompted with Error The username and password could not be verified
